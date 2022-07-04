da315bd835191df093c9bb44329882492cdcb12c
Guillaume Bordier <g_bordier@hotmail.com>
2022-07-04 18:58:42 +0100

    second commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask,request
+import re
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, Flask!"
+
+@app.route("/ddns")
+@app.route("/ddns/<name>")
+def ddns(name = ""):
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+    match_object = re.match("[a-zA-Z]+", name)
+    host=request.args.get('host')
+    ip=host=request.args.get('ip')
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "Friend"
+
+    s="ddns"
+    if (host): s=s+",host:" + host
+    if (ip): s=s+",ip:" + ip
+    s = s+ formatted_now + " " + clean_name
+    return s
